71cec086fd1d9a3a45c5798b70afd4e80959f571
ysogg <ajb.personal@hotmail.com>
2025-05-16 15:31:13 -0400

    fix?

--- src/index.tsx
+++ src/index.tsx
@@ -8,23 +8,39 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './Pages/HomePage/Home';
 import Folio from './Pages/FolioPage/Folio';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+
+
+
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    {/* ReactDOM.createRoot(root).render( */}
-  <BrowserRouter basename="/">
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="folio" element={<Folio/>} />
-      <Route path="*" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
-{/* ); */}
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="folio" element={<Folio/>} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     {/* <RouterProvider router={router} /> */}
+//     {/* ReactDOM.createRoot(root).render( */}
+//   <BrowserRouter basename="/">
+//     <Routes>
+//       <Route index element={<Home />} />
+//       <Route path="folio" element={<Folio/>} />
+//       <Route path="*" element={<Home />} />
+//     </Routes>
+//   </BrowserRouter>
+// {/* ); */}
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
