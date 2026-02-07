cd7db3da2dd6dab0d642728edfb0bcb45dfa267d
ysogg <ajb.personal@hotmail.com>
2026-02-07 13:22:37 -0500

    Much cleaner starting example

--- src/Pages/FolioPage/Folio.tsx
+++ src/Pages/FolioPage/Folio.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import WebGLComponent from '../Components/Cube'
+import { Canvas } from '@react-three/fiber'
+import Cube from '../Components/Cube'
+import "../../Styles/Grid.css"
+
 
 function Folio() {
   return (
@@ -8,7 +10,13 @@ function Folio() {
         <p>
           Test Page
         </p>
-        <WebGLComponent /> 
+        <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Cube position={[-1.2, 0, 0]} />
+        <Cube position={[1.2, 0, 0]} />
+      </Canvas>
       </header>
     </div>
   );
