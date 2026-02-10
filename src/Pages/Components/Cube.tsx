82e980c5c42ffcf03acd1d4bdbfb288775cbdfd9
ysogg <ajb.personal@hotmail.com>
2026-02-10 12:20:16 -0500

    Texture loading
    Still need to pass individual file names

--- src/Pages/Components/Cube.tsx
+++ src/Pages/Components/Cube.tsx
@@ -1,24 +1,28 @@
 import * as THREE from 'three'
 import React, { useRef, useState } from 'react'
-import { useFrame, ThreeElements } from '@react-three/fiber'
+import { useFrame, ThreeElements, useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three'
 
-
-
-const Cube = (props: ThreeElements['mesh']) => {
+const Cube = (props: ThreeElements['mesh'], ) => {
 const meshRef = useRef<THREE.Mesh>(null!)
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+	useFrame((state => {
+		meshRef.current.rotation.y = state.pointer.x *0.2;
+		meshRef.current.rotation.x = state.pointer.y *0.2;
+	}))
+	const mappedTexture = useLoader(TextureLoader, "/img/cat.png");
 	return (
 	  <mesh
 		{...props}
 		ref={meshRef}
-		scale={active ? 1.5 : 1}
+		scale={active ? 0.5 : 1.5}
 		onClick={(event) => setActive(!active)}
 		onPointerOver={(event) => setHover(true)}
 		onPointerOut={(event) => setHover(false)}>
-		<boxGeometry args={[4, 2, 1]} />
-		<meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
+		<boxGeometry args={[4, 4, 0.2]} />
+		{/* <meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} /> */}
+		<meshStandardMaterial map={mappedTexture}/>
 	  </mesh>
 	)
 }
