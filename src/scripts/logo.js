b2d390096dffd8a5a365999438677f8d8539a8ca
ysogg <ajb.personal@hotmail.com>
2026-07-26 15:38:52 -0400

    quick fixes

---
+++ src/scripts/logo.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("logo");
+    button.addEventListener('click', myCustomFunction);
+});
+
+
+function myCustomFunction() {
+    alert('0_0');
+}
+
+
